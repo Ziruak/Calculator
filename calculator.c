e60fea6b0d0f0107edd2746d66f317322c6eb574
Ziruak <jimjr.nsk.0@gmail.com>
2022-05-04 22:53:37 +0700

    init commit

---
+++ calculator.c
@@ -0,0 +1,6 @@
+# include "calculator.h"
+
+int main() {
+    printf("NOT IMPLEMENTED YET\n");
+    return 0;
+}
---
+++ calculator.h
@@ -0,0 +1,7 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <string.h>
+#include <stdio.h>
+
+#endif // !CALCULATOR_H
